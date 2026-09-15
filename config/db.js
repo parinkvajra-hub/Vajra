8307610411b1e03226756f0c42327cc05f4557ce
parinkvajra-hub <parinkvajra@gmail.com>
2026-09-15 21:15:36 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://parinkvajra_db_user:parinlock2026@cluster0.kt9d734.mongodb.net/?appName=Cluster0");
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     console.log(`   Database: ${conn.connection.name}`);
     return conn;
